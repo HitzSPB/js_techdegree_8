e320e1e0acb6517d4e19950f86216c788ed2ce88
HitzSPB <daniel.oesterbro@gmail.com>
2021-09-18 13:20:56 +0200

    Added the missing pug compared to the instructions

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json(models.Book.findAll());
+  res.redirect("/books");
 });
 
 module.exports = router;
