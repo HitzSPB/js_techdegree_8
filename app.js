81a206c31e81b67f795bf28dbd641387884e2702
HitzSPB <daniel.oesterbro@gmail.com>
2021-09-19 01:13:57 +0200

    Cleaned up more code

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Usings
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const { Op } = require("sequelize");
 const Book = require('../models').Book;
 const maxBooksApage = 5;
